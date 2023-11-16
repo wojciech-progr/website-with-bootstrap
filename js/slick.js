eecd514e87694f67350542f7dd7197592e5c815e
Wojtek <wojciech.progr@gmail.com>
2023-11-16 13:19:45 +0100

    Configuring and styling Slick carousel

--- js/slick.js
+++ js/slick.js
@@ -1,5 +1,21 @@
 $('.team-carousel').slick({
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
+	arrows: false,
+	// autoplay: true,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
 })
