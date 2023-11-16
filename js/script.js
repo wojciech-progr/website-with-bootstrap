2d1222fbd3d5acff6a86b2fa0531ee29e40b1bb3
Wojtek <wojciech.progr@gmail.com>
2023-11-16 01:28:12 +0100

    Hiding menu on mobile after click on link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
-
 	// add shadow after scroll
+	const nav = document.querySelector('.navbar')
 	function addShadow() {
 		if (window.scrollY >= 30) {
 			nav.classList.add('shadow-bg')
@@ -9,6 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
 			nav.classList.remove('shadow-bg')
 		}
 	}
+
+	// hide menu on mobile after click on link
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
+	allNavItems.forEach(item =>
+		item.addEventListener('click', () => {
+			navList.classList.remove('show')
+		})
+	)
 
 	window.addEventListener('scroll', addShadow)
 })
